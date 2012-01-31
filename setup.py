cbb25c47519ffe82a61ee018ab27d23addfc52b9
Steve Losh <steve@stevelosh.com>
2012-01-31 13:46:48 -0500

    Fix long desc.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='d',
-    version='0.1.0',
+    version='0.1.1',
     author='Steve Losh',
     author_email='steve@stevelosh.com',
     packages=['d'],
@@ -10,7 +10,7 @@ setup(
     url='http://sjl.bitbucket.org/d/',
     license='MIT',
     description="Documentation generation that won't make you tear your hair out.",
-    long_description=open('README.markdown').read(),
+    long_description='Read the documentation at http://sjl.bitbucket.org/d/',
     install_requires=[
         'Markdown',
         'pyquery',
