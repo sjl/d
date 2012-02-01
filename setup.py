39d2327177866fd2422aeccc87bd652be17a3e25
Steve Losh <steve@stevelosh.com>
2012-01-31 20:10:07 -0500

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='d',
-    version='0.1.1',
+    version='0.1.2',
     author='Steve Losh',
     author_email='steve@stevelosh.com',
     packages=['d'],
