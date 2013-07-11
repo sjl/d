d97e944f5f6431068b78e47ecd56eeb878475996
Steve Losh <steve@stevelosh.com>
2013-07-10 20:06:31 -0400

    Sort chapters properly.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='d',
-    version='0.2.1',
+    version='0.2.2',
     author='Steve Losh',
     author_email='steve@stevelosh.com',
     packages=['d'],
